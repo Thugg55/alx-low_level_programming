21ebd78cfc03fcb79e2e309da9d09e8c96c50187
Thugg55 <thuggeru7@gmail.com>
2023-09-11 06:33:14 -0700

    using the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - print string in the function "puts"
+ * Description - program prints "Programming is like building
+ * multilingual puzzler"
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
