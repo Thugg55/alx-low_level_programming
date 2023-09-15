d4c0fe86dc3fab0d0ba471a4597138f69a26e182
Thugg55 <thuggeru7@gmail.com>
2023-09-14 19:33:39 -0700

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 1 to 100
+ * for muliples of three print Fizz
+ * for muktiplies of five print Buzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int p;
+
+	for (p = 1; p <= 100; p++)
+	{
+		if (((p % 3) == 0) && ((p % 5) == 0))
+			printf("FizzBuzz");
+
+		else if ((p % 3) == 0)
+			printf("Fizz");
+
+		else if ((p % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", p);
+		if (p == 100)
+			break;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
