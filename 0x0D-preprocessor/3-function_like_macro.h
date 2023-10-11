7bb4da548987d5c40bf2f8106122a9352f45bf4e
Thugg55 <thuggeru7@gmail.com>
2023-10-11 06:36:53 -0700

    absolute value corrected

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,5 @@
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-//computes the absolute value of a number, x
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x < 0) ? (x * (-1)) : (x))
 
 #endif
