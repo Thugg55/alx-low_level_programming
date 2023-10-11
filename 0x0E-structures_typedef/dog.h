46cdd18ac17db5b70c63dfdf1be2ec12e6382988
Thugg55 <thuggeru7@gmail.com>
2023-10-11 03:17:55 -0700

    putchar prototype added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,6 +22,7 @@ struct dog
 	char *owner;
 };
 
+void _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
