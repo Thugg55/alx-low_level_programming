000ec372ac4f9ff8d49cce163716cd3c5c3af25d
Thugg55 <thuggeru7@gmail.com>
2024-04-09 05:50:41 -0700

    search value in array

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - A function to linearly implement search
+ * @array: an array of elements to be search
+ * @size: total number of elements in array
+ * @value: element(value) to search for
+ * Return: first index where value is located, if value is
+ * not present in array return -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t j;
+
+	j = 0;
+	while (j < size)
+	{
+		printf("checking value in array; %d = %d\n", j, array[j]);
+		if (array[j] == value)
+			return (j);
+	}
+
+	if (!array)
+		return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+algorithms, linear and binary search
