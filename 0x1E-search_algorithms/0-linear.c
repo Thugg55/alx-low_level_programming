e29cceb090a206b02508203c35eb0e9ff1be0004
Thugg55 <thuggeru7@gmail.com>
2024-04-09 07:16:17 -0700

    task done right

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	j = 0;
 	while (j < size)
 	{
-		printf("checking value in array; [%ld] = [%d]\n", j, array[j]);
+		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
 		j++;
 		if (array[j] == value)
 			return (j);
