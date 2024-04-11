56e80454171cfce5a7696aef1f0df4cb839e74e8
Thugg55 <thuggeru7@gmail.com>
2024-04-11 05:01:15 -0700

    working on exponentioal

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
-#include "searc_algos.h"
+#include "search_algos.h"
 
 /**
- * exponenetial_search - function searches for a key using an exponeential algorithm
+ * exponetial_search - function searches for a key using an exponeential algorithm
  * @array: An array on elements conatining the key
  * @size: The total number of elements
  * @value: The key to be found
@@ -9,3 +9,21 @@
  *
  * Description: use powers of 2 as exponential rnges 
  */
+
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t j, n;
+
+	if (array == NULL || !array)
+		return (-1);
+	if (array[0] == value)
+		return (0);
+	for (j = 0; j < size - 1 && array[j] <= value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+		j = j * 2;
+	}
+
+	n = j < size - 1 ? j : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", j / 2, n);
+
