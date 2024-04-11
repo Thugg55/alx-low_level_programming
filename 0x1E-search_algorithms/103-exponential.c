a00eaa829015441fee4c5ed4cef79ca7b23fffe7
Thugg55 <thuggeru7@gmail.com>
2024-04-11 07:35:52 -0700

    completed

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -8,19 +8,18 @@
  * @value: Value to search for
  * Return: First index where value is located or -1
  */
-
 int bin_search_exp(int *array, size_t left, size_t right, int value)
 {
-	size_t j, m, l = left, r = right;
+	size_t i, m, l = left, r = right;
 
 	if (!array)
 		return (-1);
 	while (l <= r)
 	{
 		printf("Searching in array: %d", array[l]);
-		for (j = l + 1; j <= r; j++)
+		for (i = l + 1; i <= r; i++)
 		{
-			printf(", %d", array[j]);
+			printf(", %d", array[i]);
 		}
 		printf("\n");
 		/* Computation */
@@ -35,31 +34,30 @@ int bin_search_exp(int *array, size_t left, size_t right, int value)
 	return (-1);
 }
 
+
 /**
- * exponetial_search - function searches for a key using an exponeential algorithm
- * @array: An array on elements conatining the key
- * @size: The total number of elements
- * @value: The key to be found
- * Return: return -1, if value is not present in array or if array is null
- *
- * Description: use powers of 2 as exponential rnges
+ * exponential_search - Implement binary search
+ * @array: Array to be searched
+ * @size: Size of array
+ * @value: Value to search for
+ * Return: First index where value is located or -1
  */
 
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t j, n;
+	size_t i = 1, n;
 
-	if (array == NULL || !array)
+	if (!array)
 		return (-1);
 	if (array[0] == value)
 		return (0);
-	for (j = 0; j < size - 1 && array[j] <= value)
+	while (i < size - 1 && array[i] <= value)
 	{
-		printf("Value checked array[%ld] = [%d]\n", j, array[j]);
-		j = j * 2;
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i *= 2;
 	}
 
-	n = j < size - 1 ? j : size - 1;
-	printf("Value found between indexes [%ld] and [%ld]\n", j / 2, n);
-	return (bin_search_exp(array, j / 2, n, value));
+	n = i < size - 1 ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, n);
+	return (bin_search_exp(array, i / 2, n, value));
 }
