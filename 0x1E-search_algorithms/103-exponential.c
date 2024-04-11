2b17bb1181dd8f24c5091fb7950137437bee08ad
Thugg55 <thuggeru7@gmail.com>
2024-04-10 21:54:35 -0700

    updated my header prototype

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,11 @@
+#include "searc_algos.h"
+
+/**
+ * exponenetial_search - function searches for a key using an exponeential algorithm
+ * @array: An array on elements conatining the key
+ * @size: The total number of elements
+ * @value: The key to be found
+ * Return: return -1, if value is not present in array or if array is null
+ *
+ * Description: use powers of 2 as exponential rnges 
+ */
