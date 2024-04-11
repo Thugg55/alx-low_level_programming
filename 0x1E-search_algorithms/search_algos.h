2c5b20ec208b712a4a6f04aada20759ff78e490f
Thugg55 <thuggeru7@gmail.com>
2024-04-11 07:31:31 -0700

    adding prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,6 +13,8 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int bin_search_exp(int *array, size_t left, size_t right, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
 
